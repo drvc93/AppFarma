31f52a0867ab3ed2a090e66470662fa6278f8803
DANIEL <DANIEL@DESKTOP-M9S7TBH>
2019-08-11 18:48:02 -0500

    sql script

--- AppFarma_Backup_2018.11.27_10.49.35/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Variable", "JustCode_ImplicitTypedVariableDeclarationMisused:Not implicitly typed local variable", Justification = "<Pendiente>", Scope = "member", Target = "~M:AppFarma.Form1.bbtnCategoria_ItemClick(System.Object,DevExpress.XtraBars.ItemClickEventArgs)")]
-
